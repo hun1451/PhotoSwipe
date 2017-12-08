8abf702150e4e1751f9664388c0bfba32d2516e9
김희환 <hwany1008@gmail.com>
2017-12-08 11:43:17 +0900

    데모사이트 서버 구현

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 var express =   require('express');
 var app     =   express();
+var router  =   require('./router/main')(app);
+var PhotoSwipe = require('./dist/photoswipe.js');
+
+app.set('views',__dirname + '/website');
+app.set('view engine','ejs');
+app.engine('html',require('ejs').renderFile);
 var server  =   app.listen(80, function(){
     console.log('express server has started on port 80')
 });
+
+app.use(express.static('website'));
